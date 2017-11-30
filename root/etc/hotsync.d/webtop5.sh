ca9d7c0810885cfdc941bac8ade627a92a2f446b
Stefano Fancello <stefano.fancello@nethesis.it>
2017-11-30 15:10:45 +0100

    Add external script for Webtop 5 rsync backup

---
+++ root/etc/hotsync.d/webtop5.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# source configuration file
+[ -f /etc/hotsync.conf ] && source /etc/hotsync.conf
+
+INCLUDE_FILE=$1
+EXCLUDE_FILE=$2
+
+
+# check that nethserver-webtop5 RPM is installed
+if rpm -q --quiet nethserver-webtop5; then
+    # check if postgresql rsync is enabled
+    if [[ $MYSQL != 'disabled' ]]; then
+        /usr/bin/su - postgres -c "pg_dump webtop5 > /var/lib/nethserver/webtop/backup/webtop.sql"
+        echo "/var/lib/nethserver/webtop/backup/webtop.sql" >> ${INCLUDE_FILE}
+    fi
+fi
