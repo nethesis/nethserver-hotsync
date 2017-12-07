17c2c72d0cfa8dc37149917d4e24611528b37de0
Stefano Fancello <stefano.fancello@nethesis.it>
2017-12-07 10:15:38 +0100

    Add nethserver-mattermost sync

---
+++ root/etc/hotsync.d/mattermost.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#
+# Copyright (C) 2017 Nethesis S.r.l.
+# http://www.nethesis.it - nethserver@nethesis.it
+#
+# This script is part of NethServer.
+#
+# NethServer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License,
+# or any later version.
+#
+# NethServer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NethServer.  If not, see COPYING.
+#
+
+# source configuration file
+[ -f /etc/hotsync.conf ] && source /etc/hotsync.conf
+
+INCLUDE_FILE=$1
+EXCLUDE_FILE=$2
+
+
+# check that nethserver-webtop5 RPM is installed
+if rpm -q --quiet nethserver-mattermost; then
+    # check if postgresql rsync is enabled
+    if [[ $DATABASES != 'disabled' ]]; then
+	/usr/bin/su - postgres -c "scl enable rh-postgresql94 -- pg_dump --port 55432 mattermost > /var/lib/nethserver/mattermost/backup/mattermost.sql"
+        echo "/var/lib/nethserver/mattermost/backup/mattermost.sql" >> ${INCLUDE_FILE}
+    fi
+fi
