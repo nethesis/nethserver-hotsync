bd318e38b5136c16af89f3762893389f9f87e8ed
Stefano Fancello <stefano.fancello@nethesis.it>
2017-12-11 10:39:17 +0100

    README.md -> README.rst and /etc/hotsync.d/README fixes

--- root/etc/hotsync.d/mysql.sh
+++ root/etc/hotsync.d/mysql.sh
@@ -31,9 +31,9 @@ if rpm -q --quiet nethserver-mysql; then
     if [[ $DATABASES != 'disabled' ]]; then
         # dump mysql tables
         /etc/e-smith/events/actions/mysql-dump-tables
-	#include mysql password file
-	echo "/etc/my.pwd" >> ${INCLUDE_FILE}
-	#include mysql backup
+        #include mysql password file
+        echo "/etc/my.pwd" >> ${INCLUDE_FILE}
+        #include mysql backup
         echo "/var/lib/nethserver/backup/mysql/" >> ${INCLUDE_FILE}
     fi
 fi
