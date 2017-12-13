0995f75050e6a821be71ea931d7bc5f4d6a04e8a
Stefano Fancello <stefano.fancello@nethesis.it>
2017-12-13 16:09:04 +0100

    remove obsolete /etc/my.pwd from mysql backup

--- root/etc/hotsync.d/mysql.sh
+++ root/etc/hotsync.d/mysql.sh
@@ -31,8 +31,6 @@ if rpm -q --quiet nethserver-mysql; then
     if [[ $DATABASES != 'disabled' ]]; then
         # dump mysql tables
         /etc/e-smith/events/actions/mysql-dump-tables
-        #include mysql password file
-        echo "/etc/my.pwd" >> ${INCLUDE_FILE}
         #include mysql backup
         echo "/var/lib/nethserver/backup/mysql/" >> ${INCLUDE_FILE}
     fi
