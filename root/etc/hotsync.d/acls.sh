9df3bae2f80a095d0faec48253fe974f4af1ff14
Stefano Fancello <stefano.fancello@nethesis.it>
2018-03-20 15:36:01 +0100

    Dump and save ACLs in a separate script

---
+++ root/etc/hotsync.d/acls.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#
+# Copyright (C) 2017 Nethesis S.r.l.
+# http://www.nethesis.it - nethserver@nethesis.it
+#
+# This script is part of NethServer.
+#
+# NethServer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License,
+# or any later version.
+#
+# NethServer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NethServer.  If not, see COPYING.
+#
+
+# source configuration file
+[ -f /etc/hotsync.conf ] && source /etc/hotsync.conf
+
+INCLUDE_FILE=$1
+EXCLUDE_FILE=$2
+
+if [[ -x /etc/e-smith/events/actions/nethserver-ibays-dump-acls ]]; then
+    /etc/e-smith/events/actions/nethserver-ibays-dump-acls
+    #copy acls file
+    echo "/var/lib/nethserver/backup/acls.dump" >> ${INCLUDE_FILE}
+fi
