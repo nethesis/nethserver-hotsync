63b015f09835b433f7f2c4203759c319ca9a48fd
federicoballarini <fed.ballarini@gmail.com>
2020-02-08 21:22:41 +0100

    script. add getmail sync
    Added getmail folder sync to avoid emails re-download.

---
+++ root/etc/hotsync.d/getmail.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#
+# Copyright (C) 2020 Nethesis S.r.l.
+# http://www.nethesis.it - nethserver@nethesis.it
+#
+# This script is part of NethServer.
+#
+# NethServer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License,
+# or any later version.
+#
+# NethServer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NethServer.  If not, see COPYING.
+#
+
+# source configuration file
+[ -f /etc/hotsync.conf ] && source /etc/hotsync.conf
+
+INCLUDE_FILE=$1
+EXCLUDE_FILE=$2
+
+
+# check that nethserver-mail-getmail RPM is installed
+if rpm -q --quiet nethserver-mail-getmail; then
+    # add getmail folder to backup list
+    echo "/var/lib/getmail/" >> ${INCLUDE_FILE}
+fi
